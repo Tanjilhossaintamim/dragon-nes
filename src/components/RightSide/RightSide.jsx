1aae8d79f08dd64432249c78335532b3ae260282
Tanjil Hossain <tanjilhossain845.com>
2023-10-04 12:55:49 +0600

    compleate find us section

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,4 +1,5 @@
 import Button from "../Button/Button";
+import FindUs from "../FindUs/FindUs";
 import Heading from "../Heading/Heading";
 import { AiOutlineGoogle, AiFillGithub } from "react-icons/ai";
 
@@ -6,10 +7,11 @@ const RightSide = () => {
   return (
     <div>
       <Heading title="Login With" />
+      {/* login buttons */}
       <div className="mt-5 flex justify-center flex-col gap-3">
         <Button
           name={"Login With Google"}
-          color={"blue"}
+          color={"#1569C7"}
           icon={<AiOutlineGoogle size={16} />}
           action={() => console.log("Pressed")}
         />
@@ -20,6 +22,8 @@ const RightSide = () => {
           action={() => console.log("Pressed")}
         />
       </div>
+      {/* find us/social link */}
+      <FindUs />
     </div>
   );
 };
