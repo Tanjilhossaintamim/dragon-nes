f7c8e3d62ad678405abe9f4e40b1b13063c72684
Tanjil Hossain <tanjilhossain845.com>
2023-10-04 11:49:06 +0600

    compleate middel section

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,5 +1,8 @@
+import PropTypes from "prop-types";
 const Heading = ({ title }) => {
   return <h1 className="text-xl text-color-dark-2 font-semibold">{title}</h1>;
 };
-
+Heading.propTypes = {
+  title: PropTypes.string,
+};
 export default Heading;
